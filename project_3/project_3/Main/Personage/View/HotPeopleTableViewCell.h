12a69c809a3eb694aa46dc29cfa9f2be8e57684a
Imagine <Imagine@imagine.local>
2016-09-21 17:40:51 +0800

    duitang

---
+++ project_3/project_3/Main/Personage/View/HotPeopleTableViewCell.h
@@ -0,0 +1,35 @@
+//
+//  HotPeopleTableViewCell.h
+//  project_3
+//
+//  Created by mac on 16/8/24.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HotPeopleCellModel.h"
+@class WXLabel;
+@class HotPeopleLayout;
+
+
+@interface HotPeopleTableViewCell : UITableViewCell
+
+@property(nonatomic,strong)UIImageView *icon;
+//名字
+@property(nonatomic,strong)UILabel *username;
+//简介
+@property (nonatomic,strong)WXLabel *self_description;
+//擅长
+@property(nonatomic,strong)WXLabel *love;
+//官方认证
+@property (nonatomic,strong)WXLabel *daren_description;
+//关注按钮
+@property ( nonatomic,strong)UIButton *attent;
+//头衔头像（右下角黄色）
+@property ( nonatomic,strong)UIImageView *identity;
+
+
+//布局文件
+@property (nonatomic,strong)HotPeopleLayout *layout;
+
+@end
